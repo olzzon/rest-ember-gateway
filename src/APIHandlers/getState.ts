af4f2f7c7842489631be16619ff1676de13bcd55
olzzon <olzzon@Kaspers-MacBook-Pro.local>
2020-03-05 11:16:34 +0100

    chore: add files from refactor

---
+++ src/APIHandlers/getState.ts
@@ -0,0 +1,13 @@
+export const getState = (req: any, res: any) => {
+    console.log('Query : ', req.query)
+        if (typeof(req.query.full)!=='undefined') {
+            res.json(global.emberClientStore)
+        } else if (typeof(req.query.path) !== 'undefined') {
+            global.mainThreadHandler.emberClientConnection.updatePath(req.query.path)
+            .then(()=>{
+            let pathArray = req.query.path.split('/')
+            let test = global.mainThreadHandler.emberClientConnection.getObjectFromArray(global.emberClientStore, pathArray, 0)
+            res.json(test)
+            })
+        }
+}
