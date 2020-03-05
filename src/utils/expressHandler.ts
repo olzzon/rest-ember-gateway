8f93140642c8feb3c701134a7ee71047317a668b
olzzon <olzzon@Kaspers-MacBook-Pro.local>
2020-03-05 11:15:54 +0100

    chore: refactor express server and split handlers to seperate files

--- src/utils/expressHandler.ts
+++ src/utils/expressHandler.ts
@@ -1,4 +1,7 @@
 import { logger } from './logger'
+import { getState  } from '../APIHandlers/getState'
+import { setValue  } from '../APIHandlers/setValue'
+
 
 const express = require('express')
 const path = require('path')
@@ -6,44 +9,28 @@ const app = express();
 const server = require('http').Server(app);
 const socketServer = require('socket.io')(server);
 
-app.use( '/' , express.static(path.join(__dirname ,'..')))
-server.listen(80);
-
-server.on('connection', () => {
-    app.get('/', (req: any, res: any) => {
-      console.log(req.params)
-      res.send('Access REST api by calling /state, queries: ?full  and ?path=the/tree/of/our/ember')
-    })
-    .get('/state', (req: any, res: any) => {
-      console.log('Query : ', req.query)
-      if (typeof(req.query.full)!=='undefined') {
-        res.json(global.emberClientStore)
-      } else if (typeof(req.query.path) !== 'undefined') {
-        global.mainThreadHandler.emberClientConnection.updatePath(req.query.path)
-        .then(()=>{
-          let pathArray = req.query.path.split('/')
-          let test = global.mainThreadHandler.emberClientConnection.getObjectFromArray(global.emberClientStore, pathArray, 0)
-          res.json(test)
-        })
-      }
-    })
-    .post('/setvalue', (req: any, res: any) => {
-      console.log('Query : ', req.query)
-      if (typeof(req.query.path) !== 'undefined') {
-        global.mainThreadHandler.emberClientConnection.setValue(req.query.path, req.query.value)
-        res.send('Value Changed')
-    }
-    })
-  })
-  
-socketServer.on('connection', ((socket: any) => {
-      logger.info('Client connected :' + String(socket.client.id), {})
-      // global.mainThreadHandler.socketServerHandlers(socket)
-    })
-)
-
 export const expressInit = () => {
     logger.info('Initialising WebServer')
+    app.use( '/' , express.static(path.join(__dirname ,'..')))
+    server.listen(80)
+    server.on('connection', () => {
+        app.get('/', (req: any, res: any) => {
+            console.log(req.params)
+            res.send('Access REST api by calling /state, queries: ?full  and ?path=the/tree/of/our/ember')
+        })
+        .get('/state', (req: any, res: any) => {
+            getState(req, res)
+        })
+        .post('/setvalue', (req: any, res: any) => {
+            setValue(req, res)
+        })
+    })
+
+    socketServer.on('connection', ((socket: any) => {
+        logger.info('Client connected :' + String(socket.client.id), {})
+        // global.mainThreadHandler.socketServerHandlers(socket)
+      })
+    )
 }
 
 export { socketServer }
